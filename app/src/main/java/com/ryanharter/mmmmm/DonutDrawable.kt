3aba7af517536cee0bb36a9551a7c8b2571a1b44
Jeffrey Liu <jeffreyliu8@gmail.com>
2018-07-27 15:29:07 -0700

    indent

--- app/src/main/java/com/ryanharter/mmmmm/DonutDrawable.kt
+++ app/src/main/java/com/ryanharter/mmmmm/DonutDrawable.kt
@@ -8,158 +8,161 @@ import java.util.*
 
 class DonutDrawable(sprinkleCount: Int) : Drawable() {
 
-  /*
-   * Base
-   */
-  private val basePaint = Paint().apply {
-    color = 0xFFc6853b.toInt()
-  }
-  var baseColor: Int
-    get() = basePaint.color
-    set(value) {
-      basePaint.color = value
-      invalidateSelf()
+    /*
+     * Base
+     */
+    private val basePaint = Paint().apply {
+        color = 0xFFc6853b.toInt()
     }
-
-  var scale = 0.8f
-    set(value) {
-      field = value
-      invalidateSelf()
-    }
-
-  /**
-   * Whether this is a whole donut, or a donut hole.
-   */
-  var isHole = false
-    set(value) {
-      field = value
-      invalidateSelf()
-    }
-  private val holePath = Path()
-
-  /*
-   * Icing
-   */
-  private val icingPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-    pathEffect = ComposePathEffect(CornerPathEffect(40f), DiscretePathEffect(60f, 25f))
-    color = 0xFF53250F.toInt()
-  }
-  var icingColor: Int
-    get() = icingPaint.color
-    set(value) {
-      icingPaint.color = value
-      invalidateSelf()
-    }
-
-  /*
-   * Sprinkles
-   */
-  private data class Sprinkle(val color: Int, val angle: Float, val distance: Float, var rotation: Float)
-  private val sprinkleColors = intArrayOf(Color.RED, Color.WHITE, Color.YELLOW, Color.BLUE, Color.CYAN, Color.GREEN, Color.MAGENTA)
-  private val sprinkles = generateSprinkles(sprinkleCount)
-  private val sprinklePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-  @get:Keep @set:Keep var sprinkleRotation = 0f
-    set(value) {
-      field = value
-      invalidateSelf()
-    }
-
-  private fun generateSprinkles(sprinkleCount: Int): List<Sprinkle> {
-    val random = Random()
-    return mutableListOf<Sprinkle>().also {
-      for (i in 0 until sprinkleCount) {
-        it += Sprinkle(
-            color = sprinkleColors[i % sprinkleColors.size],
-            angle = random.nextFloat() * 360f,
-            distance = random.nextFloat(),
-            rotation = random.nextFloat() * 360f
-        )
-      }
-    }
-  }
-
-  override fun onBoundsChange(bounds: Rect?) {
-    super.onBoundsChange(bounds)
-
-    // Update the clip path to be a circle that is 1/3 the diameter of the donut
-    bounds?.let {
-      holePath.reset()
-      holePath.addCircle(it.exactCenterX(), it.exactCenterY(), it.width() / 6F, Path.Direction.CW)
-    }
-  }
-
-  override fun draw(canvas: Canvas?) {
-    canvas?.let { canvas ->
-      val saveCount = canvas.save()
-      canvas.scale(scale, scale,
-          bounds.width() / 2f,
-          bounds.height() / 2f)
-
-      // clip the drawing to inside/outside the hole
-      if (isHole) {
-        // draw inside the hole
-        canvas.clipPath(holePath)
-      } else {
-        // draw outside the hole
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-          canvas.clipOutPath(holePath)
-        } else {
-          @Suppress("DEPRECATION")
-          canvas.clipPath(holePath, Region.Op.DIFFERENCE)
-        }
-      }
-
-      // draw the base
-      canvas.drawCircle(
-          bounds.exactCenterX(), // cx
-          bounds.exactCenterY(), // cy
-          bounds.width() / 2f, // radius
-          basePaint)
-
-      // draw the icing
-      canvas.drawCircle(
-          bounds.exactCenterX(),
-          bounds.exactCenterY(),
-          bounds.width() / 2.1f,
-          icingPaint)
-
-      // draw the sprinkles
-      val cx = bounds.exactCenterX()
-      val cy = bounds.exactCenterY()
-      sprinkles.forEach {
-        val holeRadius = bounds.width() / 6f
-        val padding = 20f
-
-        // set the distance based on whether this is a donut hole or a whole donut
-        val modDistance = if (isHole) {
-          holeRadius * it.distance
-        } else {
-          val ringRadius = bounds.width() / 3f
-          holeRadius + padding + (ringRadius - padding * 2) * it.distance
+    var baseColor: Int
+        get() = basePaint.color
+        set(value) {
+            basePaint.color = value
+            invalidateSelf()
         }
 
-        canvas.save()
-        canvas.rotate(it.angle, cx, cy) // rotate the entire canvas around the center
-        canvas.translate(0f, modDistance) // move the canvas to the sprinkle's position
-        canvas.rotate(it.rotation + 360f * sprinkleRotation, cx, cy) // rotate the canvas around the sprinkle's location
+    var scale = 0.8f
+        set(value) {
+            field = value
+            invalidateSelf()
+        }
 
-        sprinklePaint.color = it.color
-        canvas.drawRoundRect(cx - 7f, cy - 22f, cx + 7f, cy + 22f, 10f, 10f, sprinklePaint)
+    /**
+     * Whether this is a whole donut, or a donut hole.
+     */
+    var isHole = false
+        set(value) {
+            field = value
+            invalidateSelf()
+        }
+    private val holePath = Path()
 
-        canvas.restore()
-      }
-
-      canvas.restoreToCount(saveCount)
+    /*
+     * Icing
+     */
+    private val icingPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        pathEffect = ComposePathEffect(CornerPathEffect(40f), DiscretePathEffect(60f, 25f))
+        color = 0xFF53250F.toInt()
     }
-  }
+    var icingColor: Int
+        get() = icingPaint.color
+        set(value) {
+            icingPaint.color = value
+            invalidateSelf()
+        }
 
-  override fun setAlpha(alpha: Int) {
-    throw IllegalStateException("Who wants an invisible donut?")
-  }
+    /*
+     * Sprinkles
+     */
+    private data class Sprinkle(val color: Int, val angle: Float, val distance: Float, var rotation: Float)
 
-  override fun getOpacity(): Int = PixelFormat.TRANSLUCENT
+    private val sprinkleColors = intArrayOf(Color.RED, Color.WHITE, Color.YELLOW, Color.BLUE, Color.CYAN, Color.GREEN, Color.MAGENTA)
+    private val sprinkles = generateSprinkles(sprinkleCount)
+    private val sprinklePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    @get:Keep
+    @set:Keep
+    var sprinkleRotation = 0f
+        set(value) {
+            field = value
+            invalidateSelf()
+        }
 
-  override fun setColorFilter(colorFilter: ColorFilter?) {
-    TODO("not implemented")
-  }
+    private fun generateSprinkles(sprinkleCount: Int): List<Sprinkle> {
+        val random = Random()
+        return mutableListOf<Sprinkle>().also {
+            for (i in 0 until sprinkleCount) {
+                it += Sprinkle(
+                        color = sprinkleColors[i % sprinkleColors.size],
+                        angle = random.nextFloat() * 360f,
+                        distance = random.nextFloat(),
+                        rotation = random.nextFloat() * 360f
+                )
+            }
+        }
+    }
+
+    override fun onBoundsChange(bounds: Rect?) {
+        super.onBoundsChange(bounds)
+
+        // Update the clip path to be a circle that is 1/3 the diameter of the donut
+        bounds?.let {
+            holePath.reset()
+            holePath.addCircle(it.exactCenterX(), it.exactCenterY(), it.width() / 6F, Path.Direction.CW)
+        }
+    }
+
+    override fun draw(canvas: Canvas?) {
+        canvas?.let { canvas ->
+            val saveCount = canvas.save()
+            canvas.scale(scale, scale,
+                    bounds.width() / 2f,
+                    bounds.height() / 2f)
+
+            // clip the drawing to inside/outside the hole
+            if (isHole) {
+                // draw inside the hole
+                canvas.clipPath(holePath)
+            } else {
+                // draw outside the hole
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    canvas.clipOutPath(holePath)
+                } else {
+                    @Suppress("DEPRECATION")
+                    canvas.clipPath(holePath, Region.Op.DIFFERENCE)
+                }
+            }
+
+            // draw the base
+            canvas.drawCircle(
+                    bounds.exactCenterX(), // cx
+                    bounds.exactCenterY(), // cy
+                    bounds.width() / 2f, // radius
+                    basePaint)
+
+            // draw the icing
+            canvas.drawCircle(
+                    bounds.exactCenterX(),
+                    bounds.exactCenterY(),
+                    bounds.width() / 2.1f,
+                    icingPaint)
+
+            // draw the sprinkles
+            val cx = bounds.exactCenterX()
+            val cy = bounds.exactCenterY()
+            sprinkles.forEach {
+                val holeRadius = bounds.width() / 6f
+                val padding = 20f
+
+                // set the distance based on whether this is a donut hole or a whole donut
+                val modDistance = if (isHole) {
+                    holeRadius * it.distance
+                } else {
+                    val ringRadius = bounds.width() / 3f
+                    holeRadius + padding + (ringRadius - padding * 2) * it.distance
+                }
+
+                canvas.save()
+                canvas.rotate(it.angle, cx, cy) // rotate the entire canvas around the center
+                canvas.translate(0f, modDistance) // move the canvas to the sprinkle's position
+                canvas.rotate(it.rotation + 360f * sprinkleRotation, cx, cy) // rotate the canvas around the sprinkle's location
+
+                sprinklePaint.color = it.color
+                canvas.drawRoundRect(cx - 7f, cy - 22f, cx + 7f, cy + 22f, 10f, 10f, sprinklePaint)
+
+                canvas.restore()
+            }
+
+            canvas.restoreToCount(saveCount)
+        }
+    }
+
+    override fun setAlpha(alpha: Int) {
+        throw IllegalStateException("Who wants an invisible donut?")
+    }
+
+    override fun getOpacity(): Int = PixelFormat.TRANSLUCENT
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        TODO("not implemented")
+    }
 }
--- app/src/main/java/com/ryanharter/mmmmm/MainActivity.kt
+++ app/src/main/java/com/ryanharter/mmmmm/MainActivity.kt
@@ -17,79 +17,81 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-  private data class Flavor(val name: String, @ColorInt val color: Int)
-  private var currentBaseColor = 0
-  private val baseColors = arrayOf(
-      Flavor("Plain", 0xFFC6853b.toInt()),
-      Flavor("Chocolate", 0xFF895E3E.toInt()),
-      Flavor("Red Velvet", 0xFF8F0F1A.toInt())
-  )
-  private var currentIcingColor = 0
-  private val icingColors = arrayOf(
-      Flavor("Chocolate", 0xFF53250F.toInt()),
-      Flavor("Vanilla", 0xFFEFEFEF.toInt()),
-      Flavor("Strawberry", 0xFFDC5599.toInt())
-  )
+    private data class Flavor(val name: String, @ColorInt val color: Int)
 
-  private val content: View by lazy { findViewById<View>(android.R.id.content) }
-  private val toolbar: Toolbar by lazy { findViewById<Toolbar>(R.id.toolbar) }
-  private val platter: ImageView by lazy { findViewById<ImageView>(R.id.platter) }
-  private val scale: SeekBar by lazy { findViewById<SeekBar>(R.id.scale) }
-  private val baseColor: Button by lazy { findViewById<Button>(R.id.base_color) }
-  private val icingColor: Button by lazy { findViewById<Button>(R.id.icing_color) }
-  private val holeButton: Button by lazy { findViewById<Button>(R.id.hole) }
-  private val animateButton: Button by lazy { findViewById<Button>(R.id.animate) }
+    private var currentBaseColor = 0
+    private val baseColors = arrayOf(
+            Flavor("Plain", 0xFFC6853b.toInt()),
+            Flavor("Chocolate", 0xFF895E3E.toInt()),
+            Flavor("Red Velvet", 0xFF8F0F1A.toInt())
+    )
+    private var currentIcingColor = 0
+    private val icingColors = arrayOf(
+            Flavor("Chocolate", 0xFF53250F.toInt()),
+            Flavor("Vanilla", 0xFFEFEFEF.toInt()),
+            Flavor("Strawberry", 0xFFDC5599.toInt())
+    )
 
-  private val donut = DonutDrawable(80)
-  private var animator: Animator? = null
+    private val content: View by lazy { findViewById<View>(android.R.id.content) }
+    private val toolbar: Toolbar by lazy { findViewById<Toolbar>(R.id.toolbar) }
+    private val platter: ImageView by lazy { findViewById<ImageView>(R.id.platter) }
+    private val scale: SeekBar by lazy { findViewById<SeekBar>(R.id.scale) }
+    private val baseColor: Button by lazy { findViewById<Button>(R.id.base_color) }
+    private val icingColor: Button by lazy { findViewById<Button>(R.id.icing_color) }
+    private val holeButton: Button by lazy { findViewById<Button>(R.id.hole) }
+    private val animateButton: Button by lazy { findViewById<Button>(R.id.animate) }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    setSupportActionBar(toolbar)
+    private val donut = DonutDrawable(80)
+    private var animator: Animator? = null
 
-    scale.progress = (scale.max * donut.scale).toInt()
-    scale.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-      override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-        if (!fromUser) return
-        donut.scale = progress.toFloat() / scale.max
-      }
-      override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-      override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-    })
-    baseColor.setOnClickListener {
-      currentBaseColor = (currentBaseColor + 1) % baseColors.size
-      val flavor = baseColors[currentBaseColor]
-      donut.baseColor = flavor.color
-      Toast.makeText(this, flavor.name, Toast.LENGTH_SHORT).show()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        scale.progress = (scale.max * donut.scale).toInt()
+        scale.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (!fromUser) return
+                donut.scale = progress.toFloat() / scale.max
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+        baseColor.setOnClickListener {
+            currentBaseColor = (currentBaseColor + 1) % baseColors.size
+            val flavor = baseColors[currentBaseColor]
+            donut.baseColor = flavor.color
+            Toast.makeText(this, flavor.name, Toast.LENGTH_SHORT).show()
+        }
+        icingColor.setOnClickListener {
+            currentIcingColor = (currentIcingColor + 1) % icingColors.size
+            val flavor = icingColors[currentIcingColor]
+            donut.icingColor = flavor.color
+            Toast.makeText(this, flavor.name, Toast.LENGTH_SHORT).show()
+        }
+        holeButton.setOnClickListener {
+            val value = !donut.isHole
+            donut.isHole = value
+            holeButton.text = if (value) "Hole" else "Whole"
+        }
+        animateButton.setOnClickListener {
+            val animator = animator
+            if (animator != null) {
+                animator.cancel()
+                this.animator = null
+            } else {
+                val anim = ObjectAnimator.ofFloat(donut, "sprinkleRotation", 0f, 1f)
+                anim.interpolator = LinearInterpolator()
+                anim.duration = 3000
+                anim.repeatMode = RESTART
+                anim.repeatCount = INFINITE
+                anim.start()
+                this.animator = anim
+            }
+        }
+
+        platter.setImageDrawable(donut)
     }
-    icingColor.setOnClickListener {
-      currentIcingColor = (currentIcingColor + 1) % icingColors.size
-      val flavor = icingColors[currentIcingColor]
-      donut.icingColor = flavor.color
-      Toast.makeText(this, flavor.name, Toast.LENGTH_SHORT).show()
-    }
-    holeButton.setOnClickListener {
-      val value = !donut.isHole
-      donut.isHole = value
-      holeButton.text = if (value) "Hole" else "Whole"
-    }
-    animateButton.setOnClickListener {
-      val animator = animator
-      if (animator != null) {
-        animator.cancel()
-        this.animator = null
-      } else {
-        val anim = ObjectAnimator.ofFloat(donut, "sprinkleRotation", 0f, 1f)
-        anim.interpolator = LinearInterpolator()
-        anim.duration = 3000
-        anim.repeatMode = RESTART
-        anim.repeatCount = INFINITE
-        anim.start()
-        this.animator = anim
-      }
-    }
-
-    platter.setImageDrawable(donut)
-  }
 }
